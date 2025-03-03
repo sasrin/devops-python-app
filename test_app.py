b456c44fb2e32f2666b80878fe44f57161cb5a3d
sasrin <uwaissasrin@gmail.com>
2025-03-04 00:33:20 +0530

    add

--- test_app.py
+++ test_app.py
@@ -1,5 +1,12 @@
 import pytest
-from app import get_message
+from app import app as flask_app
 
-def test_get_message():
-    assert get_message() == "Hello, DevOps!"
+@pytest.fixture
+def client():
+    flask_app.config['TESTING'] = True
+    with flask_app.test_client() as client:
+        yield client
+
+def test_hello_route(client):
+    response = client.get('/')
+    assert response.data == b"Hello, DevOps!"
